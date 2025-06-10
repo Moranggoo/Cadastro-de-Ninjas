79bee7156c1e2215758ebc243fc29e0512ec3a5a
Morrango <152505873+Moranggoo@users.noreply.github.com>
2025-06-10 13:09:21 -0300

    feat: cria as rotas do missoescontroller com mock

--- src/main/java/io/piratex/CadastroNinjas/Missoes/MissoesController.java
+++ src/main/java/io/piratex/CadastroNinjas/Missoes/MissoesController.java
@@ -1,15 +1,37 @@
 package io.piratex.CadastroNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
     @GetMapping("/boasvindasmissoes")
     public String boasVindasMissoes() {
         return "Boas vindas, esse é a primeira rota de missoes";
+    }
+
+    // POST -- Mandar uma requisicao para criar as missoes
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada";
+    }
+
+    // GET -- Mandar uma requisicao para listar as missoes
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Todas as missoes";
+    }
+
+    // PUT -- Mandar uma requisicao para alterar as missoes
+    @PutMapping("/alterarID")
+    public String alterarMissaoPorId() {
+        return "Missao id atulizada";
+    }
+
+    // DELETE -- Mandar uma requisicao para deletar uma missao
+    @DeleteMapping("/deletarID")
+    public String deletarPorId() {
+        return "Ninja deletado";
     }
 }
